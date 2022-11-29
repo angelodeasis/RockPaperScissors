292761183d784afc5dea07fc19ac893505584ab5
angelodeasis <116287252+angelodeasis@users.noreply.github.com>
2022-11-28 20:01:27 -0500

    i basically added joption dialog lol omg
    lol

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,19 +1,22 @@
 import java.util.*;
+import javax.swing.*;
 public class RockPaperScissors
 //Set up a GUI for Rock, Paper, Scissors Program
 {
    static Scanner input = new Scanner(System.in);
    public static void main(String[] args)
    {
-      int score, computer, points, repeat;
-      String computerInput, user;   
+      int score, computer, points;
+      String computerInput, user, results, repeat;   
       
       //Initialize Starting Point Count
       points = 0;
       
       //starting loop
-      repeat = 0;
-      while(repeat == 0)
+      
+      repeat = "0"; 
+      
+      while(repeat.equals("0"))
       {
          //Variable Set-Up Section
          computer = (int)(Math.random()*3+1); //randomizes Computer Input between 1 and 3
@@ -25,63 +28,104 @@ public class RockPaperScissors
             computerInput = "Scissors";
       
          //User Interface Section
+      
+         user = JOptionPane.showInputDialog("Rock, Paper or Scissors?");
+
+         /*
          System.out.print("Rock, Paper, or Scissors? " + 
             "(Must be capitalized) ");
          user = input.next();
+         */
+         
+         
+         results = "Rock, Paper, Scissors...\n                        \nYou: " + user + "         " + "COM: " + computerInput;
+         
+         /*
          System.out.println("Rock, Paper, Scissors...");
          System.out.println("                        ");
          System.out.println("You: " + user +"         " + "COM: " + computerInput);
+         */
          
+         JOptionPane.showMessageDialog(null, results,
+                                   "Rock, Paper, Scissors",
+                                JOptionPane.INFORMATION_MESSAGE);
                   
          if(user.equals(computerInput)) 
-            System.out.print("It's a tie!\nNo points given!\n");
+         JOptionPane.showMessageDialog(null, "It's a tie!\nNo points given!\n" + "Current Points: " + points,
+                                   "Rock, Paper, Scissors",
+                                JOptionPane.INFORMATION_MESSAGE);
+                  
+            
             
          else if(user.equals("Rock"))
             if(computerInput.equals("Scissors"))
             {
-               System.out.print("You win!\n1 point given!\n");
                points = points + 1;
-               System.out.print("Current Points: " + points);
+               JOptionPane.showMessageDialog(null, "You win!\n1 point given!\n" + "Current Points: " + points,
+                                   "Rock, Paper, Scissors",
+                                JOptionPane.INFORMATION_MESSAGE);
+                                
+              
             }
             else
             {
-               System.out.print("Sorry, you lose.\n1 point lost!\n");
+      
                points = points - 1;
-               System.out.print("Current Points: " + points);
+               JOptionPane.showMessageDialog(null, "Sorry, you lose.\n1 point lost!\n" + "Current Points: " + points,
+                                   "Rock, Paper, Scissors",
+                                JOptionPane.INFORMATION_MESSAGE);
+              
             }    
             
          else if(user.equals("Paper"))
             if(computerInput.equals("Rock"))
             {
-               System.out.print("You win!\n1 point given!\n");
+              
                points = points + 1;
-               System.out.print("Current Points: " + points);
+               JOptionPane.showMessageDialog(null, "You win!\n1 point given!\n" + "Current Points: " + points,
+                                   "Rock, Paper, Scissors",
+                                JOptionPane.INFORMATION_MESSAGE);
+               
             }
             else
             {
-               System.out.print("Sorry, you lose.\n1 point lost!\n");
+               
                points = points - 1;
-               System.out.print("Current Points: " + points);
+               JOptionPane.showMessageDialog(null, "Sorry, you lose.\n1 point lost!\n" + "Current Points: " + points,
+                                   "Rock, Paper, Scissors",
+                                JOptionPane.INFORMATION_MESSAGE);
+               
             }
           
          else if(user.equals("Scissors"))
             if(computerInput.equals("Paper"))
             {
-               System.out.print("You win!\n1 point given!\n");
+              
                points = points + 1;
-               System.out.print("Current Points: " + points);
+               JOptionPane.showMessageDialog(null, "You win!\n1 point given!\n" + "Current Points: " + points,
+                                   "Rock, Paper, Scissors",
+                                JOptionPane.INFORMATION_MESSAGE);
+               
             }
             else
             {
-               System.out.print("Sorry, you lose.\n1 point lost!\n");
+               
                points = points - 1;
-               System.out.println("Current Points: " + points);
+               JOptionPane.showMessageDialog(null, "Sorry, you lose.\n1 point lost!\n" + "Current Points: " + points,
+                                   "Rock, Paper, Scissors",
+                                JOptionPane.INFORMATION_MESSAGE);
+              
             }
            
-         System.out.print("\nWould you like to play again?\n" + 
+         repeat = JOptionPane.showInputDialog("\nWould you like to play again?\n" + 
                "(0 for yes or 1 for no.) ");
-         repeat = input.nextInt();
+       
+         
+        
       }
-     System.out.print("Thanks for playing!\nYour points: " + points); 
+    
+     JOptionPane.showMessageDialog(null, "Thanks for playing!\nYour points: " + points,
+                                   "Rock, Paper, Scissors",
+                                JOptionPane.INFORMATION_MESSAGE);
    }
 }
